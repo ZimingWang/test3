428e93eae0a6cf7d76794824a1446f036ef0b8f4
zimingwang <ziming.wang@west.cmu.edu>
2016-05-13 16:44:14 -0700

    sdsds

---
+++ src/main/java/sdffa.java
@@ -0,0 +1,11 @@
+/**
+ * Created by wangziming on 5/13/16.
+ */
+public class sdffa {
+
+    public String getIn(){
+      return "sdsd";
+    }
+
+}
+
---
+++ src/test/java/sdffaTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by wangziming on 5/13/16.
+ */
+public class sdffaTest {
+    @org.junit.Test
+    public void getIn() throws Exception {
+
+    }
+
+}
